79343824b18a09211fe7275919c404c9ccc233d0
cyberghost <149342849+GhoziTech@users.noreply.github.com>
2025-10-04 08:39:21 +0700

    Add files via upload

---
+++ pages/test.js
@@ -0,0 +1,8 @@
+export default function Test() {
+  return (
+    <div style={{ padding: "40px", fontFamily: "sans-serif" }}>
+      <h1>✅ Halaman Test Muncul</h1>
+      <p>Jika kamu bisa lihat halaman ini, maka routing Next.js kamu berhasil.</p>
+    </div>
+  );
+}
