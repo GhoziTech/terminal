8511a6572673afc891e9a2248304d7b6692b2d32
cyberghost <149342849+GhoziTech@users.noreply.github.com>
2025-10-04 08:39:36 +0700

    Delete osint-v7 directory

--- osint-v7/pages/test.js
+++
@@ -1,8 +0,0 @@
-export default function Test() {
-  return (
-    <div style={{ padding: "40px", fontFamily: "sans-serif" }}>
-      <h1>✅ Halaman Test Muncul</h1>
-      <p>Jika kamu bisa lihat halaman ini, maka routing Next.js kamu berhasil.</p>
-    </div>
-  );
-}
